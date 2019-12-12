07ea4de0fcf4b96d04951d90188b47c959a4e9c6
EugeneZakhno <zohan1234567@gmail.com>
2019-12-12 22:14:03 +0200

    added comments

--- src/main/java/com/trustmeenglish/core/CoreApplication.java
+++ src/main/java/com/trustmeenglish/core/CoreApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+//  Consists from 3 annotations
+//   @EnableAutoConfiguration: enable Spring Boot’s auto-configuration mechanism
+//   @ComponentScan: enable @Component scan on the package where the application is located (see the best practices)
+//   @Configuration: allow to register extra beans in the context or import additional configuration classes
+
 public class CoreApplication {
 
 	public static void main(String[] args) {
