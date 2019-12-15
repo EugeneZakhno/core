d9b422c7979c1579100cb931ebf66cbe68b36274
EugeneZakhno <zohan1234567@gmail.com>
2019-12-15 02:18:14 +0200

    added annotations

--- src/main/java/com/trustmeenglish/core/model/User.java
+++ src/main/java/com/trustmeenglish/core/model/User.java
@@ -1,12 +1,19 @@
 package com.trustmeenglish.core.model;
 
 
-import lombok.Setter;
-import lombok.Getter;
+import lombok.*;
+import javax.persistence.*;
 
 @Getter
 @Setter
+@Entity
+@Table(name  = "users")
+@AllArgsConstructor
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     private String email;
     private  String password;
---
+++ src/main/java/com/trustmeenglish/core/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.trustmeenglish.core.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+
+
+
+}
